276f55cc98e218f7a3ea50ed4e47d95cac907636
sagarwaghamode <dipakwaghamode@gmail.com>
2020-06-25 13:10:23 +0530

    Added First name Pattern

--- userregistration.sh
+++ userregistration.sh
@@ -1,4 +1,14 @@
-#!/bin/bash -x
- 
+#!/bin/bash -x 
+
 echo welcome to User registration
 
+read -p "Enter user first Name: " firstName
+
+firstNamePattern="^[A-Z]+[a-zA-Z]{2,}"
+
+if [[ $firstName =~ $firstNamePattern ]]
+then
+	echo "valid $firstName"
+else
+	echo "invalid $firstName"
+fi
